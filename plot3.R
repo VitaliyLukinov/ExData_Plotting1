a19000132c07a84ce0e28d5095c6c0c2ef4e93b1
Vitaliy Lukinov <Vitaliy.Lukinov@gmail.com>
2015-11-07 00:23:56 +0600

    Some reconstructions

--- plot3.R
+++ plot3.R
@@ -1,18 +1,36 @@
-#Read data from file
-data.hpc <- read.table("household_power_consumption.txt",header=TRUE, 
-                                  sep=";",na.strings = c("?",""))
-#I studied a several tricks haw to read not all but part of data from this reference
-#http://stackoverflow.com/questions/23197243/how-do-i-read-only-lines-that-fulfil-a-condition-from-a-csv-into-r
-#I played with some tricks but for common use i prefer to read whole file :)
-#I don't know your OS, installed packages and other important things
+## function that calculates the number of lines in the text file between to dates/times 
+## d1 and d2 are strings, same format as "Date" and "Time" of the text file
+
+## Text file should be in the same directory
+
+n_lignes <- function(d2, d1 = "16/12/2006 17:24:00") {
+  t1 <-  strptime(d1, "%d/%m/%Y %H:%M:%S")
+  t2 <-  strptime(d2, "%d/%m/%Y %H:%M:%S")
+  l <- difftime(t2,t1, units = "mins") + 1
+  l
+}
 
 
-#Convert Data and Time columns in special R format
-data.hpc$Date<-as.Date(data.hpc$Date, format = "%d/%m/%Y")
-data.hpc$Time <- strptime(paste(data.hpc$Date, data.hpc$Time), format = "%Y-%m-%d %H:%M:%S")  
+## Extract  l2 rows in the text file, beginning at l1 row
+## Add Header
 
-#According assignment we need only data from dates 2007-02-01 and 2007-02-02 
-data.hpc<-subset(data.hpc, data.hpc$Date==as.Date("2007-02-01") | data.hpc$Date==as.Date("2007-02-02"))
+extract_file <- function(l1,l2) {
+  dataset  <- read.table(file = "household_power_consumption.txt", sep = ";", header = FALSE, as.is = TRUE, skip = l1, nrows = l2, na.strings = "?")
+  head <- read.table(file = "household_power_consumption.txt", sep = ";", header = FALSE, as.is = TRUE, nrows = 1)
+  names(dataset) <- head
+  Date_time  <- paste(dataset$Date,dataset$Time, sep = " ")
+  Date_time  <- strptime(Date_time, "%d/%m/%Y %H:%M:%S")
+  dataset  <- data.frame(Date_time, dataset)
+  dataset
+}
+
+## Create plot1 with extracted data between date/time d1 and date/time d2
+## Date/Time in format character "d/m/Y H:M:S"
+## Read data from file
+
+l1  <- n_lignes("1/2/2007 00:00:00")
+l2  <- n_lignes("2/2/2007 23:59:00","1/2/2007 00:00:00")
+data.hpc <- extract_file(l1,l2)
 
 #Use png device to create resulting file 
 png(filename = "plot3.png", width = 480, height = 480)
@@ -21,11 +39,13 @@ png(filename = "plot3.png", width = 480, height = 480)
 Sys.setlocale(locale="English")
 
 # Draw picture
-with(data.hpc, plot(Time,Sub_metering_1,type="l",
-     main="",xlab="", ylab="Energy sub metering"))
-with(data.hpc, points(Time,Sub_metering_2,type="l", col="red"))
-with(data.hpc, points(Time,Sub_metering_3,type="l", col="blue"))
-legend("topright", lty=1,
+with(data.hpc, {
+     plot(Date_time,Sub_metering_1,type="l",
+     main="",xlab="", ylab="Energy sub metering")
+     points(Date_time,Sub_metering_2,type="l", col="red")
+     points(Date_time,Sub_metering_3,type="l", col="blue")
+     legend("topright", lty=1,
        col    = c("black",          "red",            "blue"), 
        legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+})
 dev.off()
